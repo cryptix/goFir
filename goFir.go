863d47a8edbe378908b5c7caa4847148111f3d47
Henry <cryptix@riseup.net>
2013-08-05 14:04:41 +0200

    Coeffs array and Input readable from struct

--- goFir.go
+++ goFir.go
@@ -23,6 +23,7 @@ package goFir
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=0;
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_clk;
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_clk_prev;
+	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[4];
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_rst;
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_tap[4];
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[4];
@@ -31,111 +32,157 @@ package goFir
 	uint64_t de_tuhh_hbubert_noiseCancel_simpleFir_y$reg;
 	int epsCycle=0;
 	int deltaCycle=0;
-	void frame49c() {
+	void frame565() {
+		uint64_t t0=1;//const1
+		uint64_t t1=t0 & 0xffll;//cast_uint[targetSize=8,currentSize=32]
+		de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[0]=t1;
+	}
+	void frame567() {
+		uint64_t t0=1;//const1
+		uint64_t t1=t0 & 0xffll;//cast_uint[targetSize=8,currentSize=32]
+		de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[1]=t1;
+	}
+	void frame59a() {
 		uint64_t t0=de_tuhh_hbubert_noiseCancel_simpleFir_rst;
-		//loadInternal[internalIdx=15]
+		//loadInternal[internalIdx=19]
 		uint64_t t1=0;//const0
 		bool t2=t0 != t1;//not_eq
 		Pred_de_tuhh_hbubert_noiseCancel_simpleFir_rst=t2;
 		Pred_de_tuhh_hbubert_noiseCancel_simpleFir_rst_update=((uint64_t) deltaCycle << 16ll) | (epsCycle & 0xFFFF);
 	}
-	void frame48c() {
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
-		//negPredicate[internalIdx=11]
-		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_tap[0];
-		//loadInternal[internalIdx=9]
-		uint64_t t3=de_tuhh_hbubert_noiseCancel_simpleFir_tap[1];
-		//loadInternal[internalIdx=3]
-		uint64_t t4=(((int64_t)(t2 + t3)) << 56) >> 56;//plus[targetSizeWithType=17]
-		uint64_t t5=de_tuhh_hbubert_noiseCancel_simpleFir_tap[2];
-		//loadInternal[internalIdx=5]
-		uint64_t t6=(((int64_t)(t4 + t5)) << 56) >> 56;//plus[targetSizeWithType=17]
-		uint64_t t7=de_tuhh_hbubert_noiseCancel_simpleFir_tap[3];
-		//loadInternal[internalIdx=7]
-		uint64_t t8=(((int64_t)(t6 + t7)) << 56) >> 56;//plus[targetSizeWithType=17]
-		int64_t c9=t8 << 56;
-		uint64_t t9=c9 >> 56;
-		//cast_int[targetSize=8,currentSize=8]
-		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_y$reg;
-		de_tuhh_hbubert_noiseCancel_simpleFir_y$reg=t9;
-		static regUpdate_t reg;
-		if (current!=t9){
-			reg.internal=7;
-			reg.offset=0;
-			regUpdates[regUpdatePos++]=reg;
-		}
+	void frame569() {
+		uint64_t t0=1;//const1
+		uint64_t t1=t0 & 0xffll;//cast_uint[targetSize=8,currentSize=32]
+		de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[2]=t1;
 	}
-	void frame494() {
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
-		//negPredicate[internalIdx=11]
+	void frame56b() {
+		uint64_t t0=1;//const1
+		uint64_t t1=t0 & 0xffll;//cast_uint[targetSize=8,currentSize=32]
+		de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[3]=t1;
+	}
+	void frame592() {
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
+		//negPredicate[internalIdx=15]
 		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_tap[0];
-		//loadInternal[internalIdx=9]
+		//loadInternal[internalIdx=13]
 		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[1];
 		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[1]=t2;
 		static regUpdate_t reg;
 		if (current!=t2){
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=1;
 			regUpdates[regUpdatePos++]=reg;
 		}
 	}
-	void frame496() {
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
-		//negPredicate[internalIdx=11]
+	void frame594() {
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
+		//negPredicate[internalIdx=15]
 		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_tap[1];
-		//loadInternal[internalIdx=3]
+		//loadInternal[internalIdx=7]
 		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[2];
 		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[2]=t2;
 		static regUpdate_t reg;
 		if (current!=t2){
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=2;
 			regUpdates[regUpdatePos++]=reg;
 		}
 	}
-	void frame47c() {
-		//posPredicate[internalIdx=11]
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
+	void frame582() {
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
+		//negPredicate[internalIdx=15]
+		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[0];
+		//loadInternal[internalIdx=0]
+		uint64_t t3=de_tuhh_hbubert_noiseCancel_simpleFir_tap[0];
+		//loadInternal[internalIdx=13]
+		uint64_t t4=(((int64_t)(t2 * t3)) << 48) >> 48;//mul[targetSizeWithType=33]
+		uint64_t t5=de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[1];
+		//loadInternal[internalIdx=1]
+		uint64_t t6=de_tuhh_hbubert_noiseCancel_simpleFir_tap[1];
+		//loadInternal[internalIdx=7]
+		uint64_t t7=(((int64_t)(t5 * t6)) << 48) >> 48;//mul[targetSizeWithType=33]
+		uint64_t t8=(((int64_t)(t4 + t7)) << 48) >> 48;//plus[targetSizeWithType=33]
+		uint64_t t9=de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[2];
+		//loadInternal[internalIdx=2]
+		uint64_t t10=de_tuhh_hbubert_noiseCancel_simpleFir_tap[2];
+		//loadInternal[internalIdx=9]
+		uint64_t t11=(((int64_t)(t9 * t10)) << 48) >> 48;//mul[targetSizeWithType=33]
+		uint64_t t12=(((int64_t)(t8 + t11)) << 48) >> 48;//plus[targetSizeWithType=33]
+		uint64_t t13=de_tuhh_hbubert_noiseCancel_simpleFir_coeffs[3];
+		//loadInternal[internalIdx=3]
+		uint64_t t14=de_tuhh_hbubert_noiseCancel_simpleFir_tap[3];
+		//loadInternal[internalIdx=11]
+		uint64_t t15=(((int64_t)(t13 * t14)) << 48) >> 48;//mul[targetSizeWithType=33]
+		uint64_t t16=(((int64_t)(t12 + t15)) << 48) >> 48;//plus[targetSizeWithType=33]
+		int64_t c17=t16 << 56;
+		uint64_t t17=c17 >> 56;
+		//cast_int[targetSize=8,currentSize=16]
+		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_y$reg;
+		de_tuhh_hbubert_noiseCancel_simpleFir_y$reg=t17;
+		static regUpdate_t reg;
+		if (current!=t17){
+			reg.internal=8;
+			reg.offset=0;
+			regUpdates[regUpdatePos++]=reg;
+		}
+	}
+	void frame596() {
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
+		//negPredicate[internalIdx=15]
+		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_tap[2];
+		//loadInternal[internalIdx=9]
+		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[3];
+		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[3]=t2;
+		static regUpdate_t reg;
+		if (current!=t2){
+			reg.internal=6;
+			reg.offset=3;
+			regUpdates[regUpdatePos++]=reg;
+		}
+	}
+	void frame598() {
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
+		//negPredicate[internalIdx=15]
+		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_x;
+		//loadInternal[internalIdx=18]
+		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[0];
+		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[0]=t2;
+		static regUpdate_t reg;
+		if (current!=t2){
+			reg.internal=6;
+			reg.offset=0;
+			regUpdates[regUpdatePos++]=reg;
+		}
+	}
+	void frame571() {
+		//posPredicate[internalIdx=15]
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
 		uint64_t t2=0;//const0
 		uint64_t t3=t2 & 0xffll;//cast_uint[targetSize=8,currentSize=32]
 		de_tuhh_hbubert_noiseCancel_simpleFir_y$reg=t3;
 		{
 			static regUpdate_t reg;
-			reg.internal=7;
+			reg.internal=8;
 			reg.offset=-1;
 			regUpdates[regUpdatePos++]=reg;
 		}
-		//writeInternal[internal=2]
+		//writeInternal[internal=6]
 		uint64_t t5=0;//const0
 		//Write to #null
 		(void)t5;
 	}
-	void frame498() {
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
-		//negPredicate[internalIdx=11]
-		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_tap[2];
-		//loadInternal[internalIdx=5]
-		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[3];
-		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[3]=t2;
-		static regUpdate_t reg;
-		if (current!=t2){
-			reg.internal=5;
-			reg.offset=3;
-			regUpdates[regUpdatePos++]=reg;
-		}
-	}
-	void frame47e() {
-		//posPredicate[internalIdx=11]
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
+	void frame573() {
+		//posPredicate[internalIdx=15]
+		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=16]
 		uint64_t t2=0;//const0
 		memset(de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg, t2, 4);
 		{
 			static regUpdate_t reg;
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=-1;
 			regUpdates[regUpdatePos++]=reg;
 		}
-		//writeInternal[internal=13]
+		//writeInternal[internal=17]
 		uint64_t t4=0;//const0
 		int a0=(int)t4;//pushAddIndex
 		uint64_t t5=0;//const0
@@ -143,11 +190,11 @@ package goFir
 		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[a0]=t6;
 		{
 			static regUpdate_t reg;
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=a0;
 			regUpdates[regUpdatePos++]=reg;
 		}
-		//writeInternal[internal=13]
+		//writeInternal[internal=17]
 		uint64_t t8=1;//const1
 		int a1=(int)t8;//pushAddIndex
 		uint64_t t9=0;//const0
@@ -155,11 +202,11 @@ package goFir
 		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[a1]=t10;
 		{
 			static regUpdate_t reg;
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=a1;
 			regUpdates[regUpdatePos++]=reg;
 		}
-		//writeInternal[internal=13]
+		//writeInternal[internal=17]
 		uint64_t t12=2;//const2
 		int a2=(int)t12;//pushAddIndex
 		uint64_t t13=0;//const0
@@ -167,11 +214,11 @@ package goFir
 		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[a2]=t14;
 		{
 			static regUpdate_t reg;
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=a2;
 			regUpdates[regUpdatePos++]=reg;
 		}
-		//writeInternal[internal=13]
+		//writeInternal[internal=17]
 		uint64_t t16=0x3ll;//loadConstant[constantIdx=0]
 		int a3=(int)t16;//pushAddIndex
 		uint64_t t17=0;//const0
@@ -179,28 +226,14 @@ package goFir
 		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[a3]=t18;
 		{
 			static regUpdate_t reg;
-			reg.internal=5;
+			reg.internal=6;
 			reg.offset=a3;
 			regUpdates[regUpdatePos++]=reg;
 		}
-		//writeInternal[internal=13]
+		//writeInternal[internal=17]
 		uint64_t t20=0;//const0
 		//Write to #null
 		(void)t20;
-	}
-	void frame49a() {
-		de_tuhh_hbubert_noiseCancel_simpleFir_clk_update=((uint64_t) deltaCycle << 16l) | (epsCycle & 0xFFFF);//isRisingEdge[internalIdx=12]
-		//negPredicate[internalIdx=11]
-		uint64_t t2=de_tuhh_hbubert_noiseCancel_simpleFir_x;
-		//loadInternal[internalIdx=14]
-		uint64_t current=de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[0];
-		de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[0]=t2;
-		static regUpdate_t reg;
-		if (current!=t2){
-			reg.internal=5;
-			reg.offset=0;
-			regUpdates[regUpdatePos++]=reg;
-		}
 	}
 	bool skipEdge(uint64_t local) {
 		uint64_t dc = local >> 16l;
@@ -219,12 +252,12 @@ package goFir
 		for (i=0;i<regUpdatePos; i++) {
 			regUpdate_t reg=regUpdates[i];
 			switch (reg.internal) {
-				case 5:
+				case 6:
 				if (reg.offset==-1)
 					memcpy(de_tuhh_hbubert_noiseCancel_simpleFir_tap, de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg, 4);
 				else
 					de_tuhh_hbubert_noiseCancel_simpleFir_tap[reg.offset] = de_tuhh_hbubert_noiseCancel_simpleFir_tap$reg[reg.offset]; break;
-				case 7:
+				case 8:
 				de_tuhh_hbubert_noiseCancel_simpleFir_y = de_tuhh_hbubert_noiseCancel_simpleFir_y$reg; break;
 			}
 		}
@@ -234,39 +267,43 @@ package goFir
 		epsCycle=0;
 		do {
 			regUpdatePos=0;
-		frame49c();
+		frame565();
+		frame567();
+		frame59a();
+		frame569();
+		frame56b();
 		bool de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising=true;
 		bool de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled=false;
 		if (!disableEdges){
-			uint64_t t12=de_tuhh_hbubert_noiseCancel_simpleFir_clk;
-			uint64_t t12_prev=de_tuhh_hbubert_noiseCancel_simpleFir_clk_prev;
-			if ((t12_prev!=0) || (t12!=1)) {
+			uint64_t t16=de_tuhh_hbubert_noiseCancel_simpleFir_clk;
+			uint64_t t16_prev=de_tuhh_hbubert_noiseCancel_simpleFir_clk_prev;
+			if ((t16_prev!=0) || (t16!=1)) {
 				de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising=false;
 			}
 		}
 		if (skipEdge(de_tuhh_hbubert_noiseCancel_simpleFir_clk_update)){
 			de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled=true;
 		}
-		bool p11=Pred_de_tuhh_hbubert_noiseCancel_simpleFir_rst;
-		bool p11_fresh=true;
-		uint64_t up11=Pred_de_tuhh_hbubert_noiseCancel_simpleFir_rst_update;
-		if ((up11>>16 != deltaCycle) || ((up11&0xFFFF) != epsCycle)){
-			p11_fresh=false;
+		bool p15=Pred_de_tuhh_hbubert_noiseCancel_simpleFir_rst;
+		bool p15_fresh=true;
+		uint64_t up15=Pred_de_tuhh_hbubert_noiseCancel_simpleFir_rst_update;
+		if ((up15>>16 != deltaCycle) || ((up15&0xFFFF) != epsCycle)){
+			p15_fresh=false;
 		}
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p11 && p11_fresh)
-			frame48c();
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p11 && p11_fresh)
-			frame494();
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p11 && p11_fresh)
-			frame496();
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && p11 && p11_fresh)
-			frame47c();
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p11 && p11_fresh)
-			frame498();
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && p11 && p11_fresh)
-			frame47e();
-		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p11 && p11_fresh)
-			frame49a();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p15 && p15_fresh)
+			frame592();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p15 && p15_fresh)
+			frame594();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p15 && p15_fresh)
+			frame582();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p15 && p15_fresh)
+			frame596();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && !p15 && p15_fresh)
+			frame598();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && p15 && p15_fresh)
+			frame571();
+		if (de_tuhh_hbubert_noiseCancel_simpleFir_clk_isRising&& !de_tuhh_hbubert_noiseCancel_simpleFir_clk_risingIsHandled && p15 && p15_fresh)
+			frame573();
 		updateRegs();
 		epsCycle++;
 		} while (regUpdatePos!=0 && !disabledRegOutputlogic);
@@ -281,6 +318,7 @@ import (
 )
 
 type SimpleFirSim struct {
+	Input    []int
 	Data     []State
 	Duration time.Duration
 }
@@ -351,6 +389,7 @@ func RunSimulation(input []int) *SimpleFirSim {
 
 	var data []State
 
+	runner.Input = input
 	// TODO check if anoter sim is running!
 
 	// starting simulation
